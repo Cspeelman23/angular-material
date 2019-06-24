d4a8f677b731b02b55efa65796c24adee595deba
Cady Speelman <cady.speelman@travelport.com>
2019-06-24 17:03:37 -0600

    added http/json response to Matdemo Page 6

--- src/app/ffshop.service.ts
+++ src/app/ffshop.service.ts
@@ -8,14 +8,14 @@ import {Observable} from 'rxjs';
 })
 
 export class FFShopService {
-  //private dataUrl = '' //web api url, :base/:collectionName
+  //private dataUrl = 'http://my-json-server.typicode.com/Cspeelman23/json-data/'; //web api url, :base/:collectionName
   constructor(private http: HttpClient) {
     this.getJSON().subscribe(data => {
         console.log(data);
     });
   }
   public getJSON(): Observable<any> {
-    return this.http.get<any>("FFSResponse.json");
+    return this.http.get("http://jsonplaceholder.typicode.com/users/1");
     //return this.http.get<any>("this.dataUrl");
     //return this.http.get("./assets/mydata.json");
   }
--- src/app/mat-form-field6/mat-form-field6.component.ts
+++ src/app/mat-form-field6/mat-form-field6.component.ts
@@ -4,23 +4,46 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
 import { FFShopService } from '../ffshop.service';
 import {Observable} from 'rxjs';
 
-
+export interface TestData {
+  id: number;
+  name: string;
+  email: string;
+  address;
+}
 @Component({
   selector: 'app-mat-form-field6',
   templateUrl: './mat-form-field6.component.html',
   styleUrls: ['./mat-form-field6.component.css']
 })
+
+
 export class MatFormField6Component implements OnInit {
   title = 'Matdemo Page 6';
+  result= '';
+  testy: TestData;
   FFSResponse: any;
   constructor(FFSService: FFShopService) { 
-    FFSService.getJSON().subscribe(data => {
-      console.log(data);
-    });
-  
- }
+    FFSService.getJSON().subscribe(testy => this.testy =testy)   
+  }
+
+    CallOnMe(x){
+      if (x==1){
+        console.log("testy = " + this.testy.name);
+        this.result= this.testy.name;
+      }
+      else if(x==2){
+        console.log("testy = " + this.testy.email);
+        this.result= this.testy.email;
+      }else{
+        console.log("testy = " + this.testy.address.city);
+        this.result= this.testy.address.city;
+      }
+    }
+ 
 
   ngOnInit() {
   }
+
+  
 
 }
